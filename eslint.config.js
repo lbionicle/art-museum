6ffa78ba23eeea4f645fa462d493d8738e790eb6
Nikolay Tsymbal <kolya.tsymbal45@gmail.com>
2025-02-20 00:08:10 +0300

    chore: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['**/*.test.tsx', '**/*.test.ts', 'dist/**'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': 'warn',
+      'react-hooks/exhaustive-deps': 'error',
+    },
+  },
+
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    env: {
+      jest: true,
+    },
+    rules: {
+      'testing-library/no-dom-import': 'off',
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
+
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
