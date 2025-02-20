ffab2043ec734570ed74869823591ea4779e7e2c
Nikolay Tsymbal <kolya.tsymbal45@gmail.com>
2025-02-20 19:14:52 +0300

    feat: add searchResults component and styles

--- src/components/SearchResults/index.tsx
+++ src/components/SearchResults/index.tsx
@@ -1,3 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import Table from '../Table';
+import { useSearchParams } from 'react-router-dom';
+import { searchArtwork } from '@/api/route';
+
 export default function SearchResults() {
-  return <></>;
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const [searchParams, _] = useSearchParams();
+  const query = searchParams.get('search') || '';
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['searchArtwork', query],
+    queryFn: () => searchArtwork(query),
+    enabled: query.trim().length > 0,
+  });
+
+  if (isLoading) return;
+  if (error) return;
+
+  return <Table cards={data ?? []} variant="small" />;
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,14 @@
 import MainPromo from '@/components/Promo';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import SearchResults from '@/components/SearchResults';
 
 export default function MainPage() {
   return (
     <>
       <Header />
       <MainPromo />
+      <SearchResults />
       <Footer />
     </>
   );
