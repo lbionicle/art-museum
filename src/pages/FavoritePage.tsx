606a7a00827352506fee76ee03cebe66785ebf39
Nikolay Tsymbal <kolya.tsymbal45@gmail.com>
2025-02-21 04:02:46 +0300

    feat: added layout component to the pages

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,13 +1,10 @@
 import ArtFav from '@/components/ArtFav';
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
+import Layout from '@/components/Layout';
 
 export default function FavoritePage() {
   return (
-    <>
-      <Header />
+    <Layout>
       <ArtFav />
-      <Footer />
-    </>
+    </Layout>
   );
 }
