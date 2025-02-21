1ecb613461fd4ab33b0e9db6c57a52b40aeb9028
Nikolay Tsymbal <kolya.tsymbal45@gmail.com>
2025-02-21 19:23:23 +0300

    feat: added 404 page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { ArtPage, FavoritePage, MainPage } from '@/pages';
+import { ArtPage, FavoritePage, MainPage, Page404 } from '@/pages';
 
 export default function App() {
   const client = new QueryClient();
@@ -13,6 +13,7 @@ export default function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/artworks/:id" element={<ArtPage />} />
           <Route path="/user/favorites" element={<FavoritePage />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </Router>
     </QueryClientProvider>
